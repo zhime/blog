32fed32617bc1f0ad10f347843c2bf7e6fc3d382
zhangyunfei <931013000@qq.com>
2023-03-28 21:51:02 +0800

    init

--- server/main.go
+++ server/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("test")
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"code": "0",
+			"msg":  "success",
+			"data": "",
+		})
+	})
+
+	_ = router.Run()
 }
