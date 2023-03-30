7b458e770ae5c7d9b599cd200ccb25eb6e5d1aaa
zhangyunfei <931013000@qq.com>
2023-03-30 15:59:43 +0800

    update model

--- server/initialize/gorm.go
+++ server/initialize/gorm.go
@@ -9,8 +9,21 @@ import (
 )
 
 func RegisterTables(db *gorm.DB) {
-	err := db.AutoMigrate(
-		model.User{},
+
+	_ = db.SetupJoinTable(&model.UserModel{}, "CollectModel", &model.UserCollectModel{})
+	_ = db.SetupJoinTable(&model.MenuModel{}, "Banners", &model.MenuBannerModel{})
+	err := db.Set("gorm:table_options", "ENGINE=InnoDB").AutoMigrate(
+		&model.BannerModel{},
+		&model.TagModel{},
+		&model.MessageModel{},
+		&model.AdvertModel{},
+		&model.UserModel{},
+		&model.CommentModel{},
+		&model.ArticleModel{},
+		&model.MenuModel{},
+		&model.MenuBannerModel{},
+		&model.FadeBackModel{},
+		&model.LoginModel{},
 	)
 	if err != nil {
 		global.Logger.Error("register table failed", zap.Error(err))
